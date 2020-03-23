8ff985961490517c378aba747f9ea47de405a1c5
Mercia <merciarandrianome@gmail.com>
2020-03-23 14:45:52 +0100

    add path/api/user

--- index.js
+++ index.js
@@ -2,4 +2,16 @@ const express = require ("express");
 
 const app = express();
 
-app.listen(3000);
+app.listen(3000, () => {
+	console.log("SERVER STARTED...");
+});
+
+app.get("/api/user", (req, res) => {
+	res.send ({
+		name: "Toto",
+		verified: true,
+		birthdate: new Date(2019, 03, 17),
+		friends: [ "variant", "titi"],
+
+	});
+});
