89a641df152caab01a00db1918a39bb961874fc3
Mercia <merciarandrianome@gmail.com>
2020-03-24 12:46:19 +0100

    add package eslint fix, corrected index.js

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 const express = require ("express");
 
+const toto = "tata"; // let au lieu const
+
+if (toto === "tata") { // == au lieu de ===
+    // do something
+    console.log("Hello world"); // INDENTATION !!!!!
+}
+
 const app = express();
 
 app.listen(3000, () => {
-	console.log("SERVER STARTED...");
+    console.log("SERVER STARTED...");
 });
 
-app.use(express.static('./public'));
+app.use(express.static("./public"));
 
 app.get("/api/user", (req, res) => {
-	res.send ({
-		name: "Toto",
-		verified: true,
-		birthdate: new Date(2019, 03, 17),
-		friends: [ "variant", "titi"],
+    res.send ({
+        name: "Titi",
+        verified: true,
+        birthdate: new Date(2019, 3, 17), // AT
+        friends: ["variant", "titi"],
 
-	});
+    });
 });
-
-
-
